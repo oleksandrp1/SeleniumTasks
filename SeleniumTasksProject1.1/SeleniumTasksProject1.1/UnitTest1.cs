dc78a04a2a13f60faaf657473ad38a3c504eeb07
Oleksandr Pushchalo <pushchalo.olexandr@gmail.com>
2016-11-22 13:18:26 +0200

    Created a test for Chrome browser

--- SeleniumTasksProject1.1/SeleniumTasksProject1.1/UnitTest1.cs
+++ SeleniumTasksProject1.1/SeleniumTasksProject1.1/UnitTest1.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumTasksProject1._1
+namespace Selenium
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
         {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void OpenUrlTest()
+        {
+            driver.Url = "http://www.google.com";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Google Search"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
         }
     }
 }
