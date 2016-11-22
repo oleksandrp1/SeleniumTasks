8e357b264b481605b48fd7d134f02b9cf855c1e2
Oleksandr Pushchalo <pushchalo.olexandr@gmail.com>
2016-11-22 17:38:26 +0200

    LoginTest is added

---
+++ SeleniumTasksProject1.1/SeleniumTasksProject1.1/UnitTest2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace SeleniumTasksProject1._1
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Url = "http://localhost:8082/litecart/admin/";
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
